76fc113d884e2e6cbf95f032982fe89d3cf6bc75
sukarnozhang <sukarno.zhang@gmail.com>
2023-11-18 13:39:22 +0800

    initial attempt

---
+++ assignment/src/test/java/sg/ntu/edu/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.ntu.edu.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
